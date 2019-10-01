8a8425aacf693534974927b1a4e431f4fdebc9ba
Danny <dannydanny7833@gmail.com>
2019-10-01 20:07:38 +0900

    Solution #2

--- docs/static/js/disqus.js
+++
@@ -1,6 +0,0 @@
-function disqus() { // DON'T EDIT BELOW THIS LINE
-var d = document, s = d.createElement('script');
-s.src = 'https://cookieinteractive.disqus.com/embed.js';
-s.setAttribute('data-timestamp', +new Date());
-(d.head || d.body).appendChild(s);
-};
