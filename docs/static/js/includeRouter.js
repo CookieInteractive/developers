225087c105bd7185d8c1e201643f9d54412d49ff
Danny <dannydanny7833@gmail.com>
2019-11-10 16:56:45 +0900

    commit all

--- docs/static/js/includeRouter.js
+++ docs/static/js/includeRouter.js
@@ -1,15 +1,15 @@
 function includeRouter(cb) {
-  var content, file, xhttp, i;
+  var contents, file, xhttp, i;
   document.body.addEventListener('click', (e)=> {
     file = e.target.getAttribute('route-link');
     if (file) {
-      content = document.getElementById('content');
+      contents = document.getElementById('contents');
       xhttp = new XMLHttpRequest();
       xhttp.onreadystatechange = function() {
         if (this.readyState == 4) {
           if (this.status == 200) {
-            content.innerHTML = this.responseText;
-            var scripts = content.getElementsByTagName('script');
+            contents.innerHTML = this.responseText;
+            var scripts = contents.getElementsByTagName('script');
             for (var i = 0; i < scripts.length; i++) {
               eval(scripts[i].text);
             }
@@ -28,7 +28,7 @@ function includeRouter(cb) {
             // added for disqus comments - end
           }
           if (this.status == 404) {
-            content.innerHTML = 'Page not found.';
+            contents.innerHTML = 'Page not found.';
           }
         }
       }
